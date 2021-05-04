71a712ae9a973afd117b14e80104a2f888e4bdff
Adrians Heidens <adrians.heidens@gmail.com>
2021-05-04 20:01:30 +0300

    Variables with integer values.

---
+++ 01-variables.py
@@ -0,0 +1,16 @@
+# Variables with integer values.
+# print(x) -- writes x to output.
+
+a = 12  # Assign value 12 to variable a
+print(a)  # Print the value of a (12)
+
+b = 21  # Assign value 21 to variable b
+print(b)  # Print value of b (21)
+print(a)  # Print value of a (12)
+
+a = 2  # Assign value 2 to variable a
+print(a)  # 2
+
+b = a  # Assign value of a to variable b (2)
+print(b)  # 2
+print(a)  # 2
