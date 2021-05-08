697f029279f59ca9d697eb9b1be0006d0f6c09e5
Adrians Heidens <adrians.heidens@gmail.com>
2021-05-08 23:41:34 +0300

    numbers

---
+++ 03-numbers.py
@@ -0,0 +1,11 @@
+print(2 + 3 * 4)  # 14
+print((2 + 3) * 4)  # 20
+
+print(7 / 3)
+print(7 % 3)  # Remainder of division (1).
+print(7 // 3)  # Division discarding fractional part (2).
+
+print(2 ** 3)  # Power operator (2^3 = 8).
+
+x = 0.25  # Floating point numbers.
+print(70 * x)  # 17.5
